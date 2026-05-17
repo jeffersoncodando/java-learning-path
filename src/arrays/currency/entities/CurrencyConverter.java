c7c62a2d55c43314cdd9a11dda42834fe12e4852
Jefferson Fernando <jefferson.fernandocg@gmail.com>
2026-05-17 13:24:37 -0400

    style: reorganize repository structure with numbered folders for chronological sorting

--- src/arrays/currency/entities/CurrencyConverter.java
+++
@@ -1,9 +0,0 @@
-package arrays.currency.entities;
-
-public class CurrencyConverter {
-    public static double iofRate = 0.06;
-
-    public static double dollarToReal(double dollarPrice, double dollarAmount){
-        return dollarAmount * dollarPrice * (1.0 + iofRate);
-    }
-}
