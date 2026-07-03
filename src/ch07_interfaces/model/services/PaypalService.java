131e0f2c54049943225d0ec3323e254f444fc786
Jefferson Fernando <jefferson.fernandocg@gmail.com>
2026-07-02 20:45:53 -0400

    refactor: add interest and fee constants to PaypalService

--- src/ch07_interfaces/model/services/PaypalService.java
+++ src/ch07_interfaces/model/services/PaypalService.java
@@ -2,14 +2,16 @@ package ch07_interfaces.model.services;
 
 public class PaypalService implements OnlinePaymentService{
 
+    private static final double FEE_PERCENTAGE = 0.02;
+    private static final double MONTHLY_PERCENTAGE = 0.01;
 
     @Override
-    public double paymentFee(double amount) {
-        return amount * 0.02;
+    public double paymentFee(double amount){
+        return amount * FEE_PERCENTAGE;
     }
 
     @Override
-    public double interest(double amount, int months) {
-        return amount * 0.01 * months;
+    public double interest(double amount, int months){
+        return amount * MONTHLY_PERCENTAGE * months;
     }
 }
